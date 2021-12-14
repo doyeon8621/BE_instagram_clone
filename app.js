de8f2b54354b7c97d8ec25b527c2e1416096627f
TnIoP <pointblank94@naver.com>
2021-12-14 16:21:12 +0900

    API 기능 구현 전 init

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-const jwt = require('jsonwebtoken');
-const authMiddleware = require('./middlewares/auth-middleware');
+//const authMiddleware = require('./middlewares/auth-middleware');
 const swaggerUi = require("swagger-ui-express");
-const swaggerFile = require("./swagger-output");
+//const swaggerFile = require("./swagger-output");
+const mysql = require('mysql'); 
+const {sequelize} = require('./models');
 
-
-app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+//app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const corsOptions = {
   origin: '*', // 나중에 여기에 url
@@ -16,20 +16,27 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const postsRouter = require('./routers/posts');
-const userRouter = require('./routers/user');
-const commentRouter = require('./routers/comment');
+//db연결(무조건 필요! )
+sequelize.sync({force:false})
+  .then(()=>{
+    console.log('db Connected')
+  })
+  .catch((err)=>{
+    console.log(err);
+  });
+  
+// const postsRouter = require('./routers/posts');
+// const userRouter = require('./routers/user');
+// const commentRouter = require('./routers/comment');
 
-const connect = require('./schemas');
-connect();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.use('/api', express.urlencoded({ extended: false }), postsRouter);
-app.use('/api', express.urlencoded({ extended: false }), userRouter);
-app.use('/api', express.urlencoded({ extended: false }), commentRouter);
+// app.use('/api', express.urlencoded({ extended: false }), postsRouter);
+// app.use('/api', express.urlencoded({ extended: false }), userRouter);
+// app.use('/api', express.urlencoded({ extended: false }), commentRouter);
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
